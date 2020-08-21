56a71cd9ef9c986907051480ef9c4141ee1a8669
InesComarella <inesmottacomarella@gmail.com>
2020-08-21 18:09:51 -0300

    Plot com dados limpos do ccma

--- R/05-novos-registros-anos.R
+++ R/05-novos-registros-anos.R
@@ -3,21 +3,40 @@ library(ggplot2)
 
 setwd('./data')
 
-data_modif <- read.csv("data-clean.csv")
+# Input
+data <- read.csv("data-ccma.csv")
 
-data_modif_sorted <- arrange(data_modif, eventYear)
+# Ordena por ordem de ano
+data_sorted <- arrange(data, eventYear)
 
-earlier_register <- data_modif_sorted[!duplicated(data_modif_sorted$acceptedNameUsage, fromLast = FALSE),]
+# Mantem apenas o primeiro registro da especie
+earlier_register <- data_sorted[!duplicated(data_sorted$species, fromLast = FALSE),]
 
-table_jhow <- data.frame('year' = unique(earlier_register$eventYear),
-                         'n.species' = NA)
+# Anos de registro
+registered_per_year <- data.frame('year' = as.character(unique(earlier_register$eventYear)),
+                         'n.species' = "",
+                         stringsAsFactors = FALSE)
 
+# Adicionando numero de registros novos em cada ano
 for (i in 1:nrow(earlier_register))
-  table_jhow$n.species[i] <- nrow(earlier_register %>% filter(eventYear == table_jhow$year[i]))
+  registered_per_year$n.species[i] <- nrow(earlier_register %>% 
+                                             filter(eventYear == registered_per_year$year[i]))
 
-bar_plot <- ggplot(table_jhow, aes(year, n.species)) +
-  geom_col(width = 1)
+years <- as.character(seq(min(earlier_register$eventYear), max(earlier_register$eventYear), 1))
+registers_through_years <- data.frame('year' = years,
+                                  stringsAsFactors = FALSE)
 
+registers_through_years <- merge(registers_through_years, registered_per_year, by = 'year', all.x = TRUE)
+
+# Output
 jpeg('../figs/registros-anos.jpg')
-bar_plot
+
+barplot(
+  height = as.numeric(registers_through_years$n.species),
+  names = registers_through_years$year,
+  col = "#69b3a2",
+  horiz = F ,
+  las = 1
+)
+
 dev.off()
