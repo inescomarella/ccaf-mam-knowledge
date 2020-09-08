7b8338acf3ad5249c78d88f70e25c64f96a400b5
InesComarella <inesmottacomarella@gmail.com>
2020-09-08 09:15:49 -0300

    Renamed and standardized

--- qgis/05-create-grid.py
+++ qgis/05-create-grid.py
@@ -1,34 +1,46 @@
 from qgis import processing
 
-# parameters do create grid
+# Parameters do create grid
 layer = QgsProject.instance().mapLayersByName("ccma_clipped")[0]
 crs = layer.crs()
 crs.createFromId(4326)
-grid= "/home/ines/tcc-ccma/outputs/grid.shp"
+grid = "/home/ines/tcc-ccma/outputs/grid.shp"
 
-# create grid algorithm
-processing.run("qgis:creategrid", {'TYPE': 4,
+# Creating grid
+grid_params = {
+    'TYPE': 4,
     'HSPACING': 0.25,
     'VSPACING': 0.25,
     'EXTENT': layer,
     'CRS': crs,
-    'OUTPUT': grid})
+    'OUTPUT': grid
+    }
+    
+print("Running the creategrid algorithm...")  
+processing.run("qgis:creategrid", grid_params)
 
 # Load vector grid
 grid_layer = QgsVectorLayer(grid, "grid", "ogr")
 QgsProject.instance().addMapLayer(grid_layer)
 
-# Clip vector grid
+# Clipping vector grid
 path_to_clipped_output = "/home/ines/tcc-ccma/outputs/grid-clipped.shp"
-overlay_layer = QgsProject.instance().mapLayersByName("ccma_clipped")[0]
 
-processing.run("qgis:clip", {'INPUT': grid_layer,
-    'OVERLAY': overlay_layer,
-    'OUTPUT': path_to_clipped_output})
+clip_params = {
+    'INPUT': grid_layer,
+    'OVERLAY': layer,
+    'OUTPUT': path_to_clipped_output
+    }
+
+print("Running the clip algorithm...")  
+processing.run("qgis:clip", clip_params)
 
 # Add clipped layer
 clipped_layer = QgsVectorLayer(path_to_clipped_output, "grid_clipped", "ogr")
 QgsProject.instance().addMapLayer(clipped_layer)
 
-# Remove old layer
+# Remove unclipped layer
 QgsProject.instance().removeMapLayer(grid_layer)
+
+print("Done!")
+
