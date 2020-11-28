2e7772734fedb7a3334fd1db6a030a09ae0eed39
InesComarella <inesmottacomarella@gmail.com>
2020-11-27 23:28:40 -0300

    :tada: Upload results to Google Drive
    Create folder in google drive, update files. \n Script not checked yet

---
+++ code/R-scripts/upload-to-drive.R
@@ -0,0 +1,125 @@
+# File purpose: Upload results to Google Drive
+# Date: 27/11/2020
+
+# Load library
+library(googledrive)
+
+# Login ---------------------------------------------------------------------
+
+# Authorize googledrive to view and manage your Drive files
+drive_auth(
+  email = "inesmottacomarella@gmail.com",
+  path = NULL,
+  scopes = "https://www.googleapis.com/auth/drive",
+  cache = gargle::gargle_oauth_cache(),
+  use_oob = gargle::gargle_oob_default(),
+  token = NULL
+)
+
+# Prepare folder and files ---------------------------------------------------
+
+# Check if folders already exist and create them
+if (nrow(drive_get(path = "~/ccma-tcc/results/correlation")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/correlation")
+}
+
+if (nrow(drive_get(path = "~/ccma-tcc/results/model")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/model")
+}
+
+if (nrow(drive_get(path = "~/ccma-tcc/results/order-maps")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/order-maps")
+}
+
+if (nrow(drive_get(path = "~/ccma-tcc/results/all-mammals-maps")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/all-mammals-maps")
+}
+
+# Get files path to upload
+file_paths_correlation <-
+  list.files(
+    "../data/results",
+    pattern = "correlation",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+file_paths_model_plots <-
+  list.files(
+    "../data/results",
+    pattern = "model",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+file_paths_orders <-
+  list.files(
+    "../data/results",
+    pattern = "order",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+file_paths_all_mammals <-
+  list.files(
+    "../data/results",
+    pattern = "all",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+# Get file names by removing the path
+file_names_correlation <-
+  str_replace(file_paths_correlation, "../data/results", "")
+
+file_names_model_plots <-
+  str_replace(file_paths_model_plots, "../data/results", "")
+
+file_names_orders <-
+  str_replace(file_paths_orders, "../data/results", "")
+
+file_names_all_mammals <-
+  str_replace(file_paths_all_mammals, "../data/results", "")
+
+# Upload files ---------------------------------------------------------------
+
+# Upload all files to its respective folders
+for (i in 1:length(file_paths_correlation)) {
+  drive_upload(
+    file_paths_correlation[i],
+    name = file_names_correlation[i],
+    path = "~/ccma-tcc/results/correlation",
+    overwrite = TRUE,
+    verbose = TRUE
+  )
+}
+
+for (i in 1:length(file_paths_model_plots)) {
+  drive_upload(
+    file_paths_model_plots[i],
+    name = file_names_model_plots[i],
+    path = "~/ccma-tcc/results/model",
+    overwrite = TRUE,
+    verbose = TRUE
+  )
+}
+
+for (i in 1:length(file_paths_all_mammals)) {
+  drive_upload(
+    file_paths_all_mammals[i],
+    name = file_names_orders[i],
+    path = "~/ccma-tcc/results/all-mammals-maps",
+    overwrite = TRUE,
+    verbose = TRUE
+  )
+}
+
+for (i in 1:length(file_paths_orders)) {
+  drive_upload(
+    file_paths_orders[i],
+    name = file_names_all_mammals[i],
+    path = "~/ccma-tcc/results/order-maps",
+    overwrite = TRUE,
+    verbose = TRUE
+  )
+}
