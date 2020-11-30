29eae6eec028eebdc8fb477960c41008c8be005d
InesComarella <inesmottacomarella@gmail.com>
2020-11-30 18:31:44 -0300

    :children_crossing: Functions for animation in a separate file
    Already commented, using dot to separate words in function (as in the rest of the project). Functions already tested and working (so far)

--- code/R-scripts/06b-do-animation-plot-map.R
+++ code/R-scripts/06b-do-animation-plot-map.R
@@ -4,9 +4,11 @@
 library(tidyverse)
 library(animation)
 library(sf)
-library(rlang)
+
 
 conflicted::conflict_prefer("filter", "dplyr")
+
+source("./R-scripts/functions/06b-funs-do-animation-plot-map.R")
 
 # Load data -------------------------------------------------------------------
 record_data <-
@@ -25,90 +27,32 @@ g025_geom <-
   )
 g025_geom <- st_transform(g025_geom, sp::CRS("+proj=longlat +datum=WGS84"))
 
-# Table ----------------------------------------------------------------------
-nreg_along_years <- function(pts, map_geom) {
-  pts <-
-    pts %>%
-    filter(year != "NA") %>%
-    mutate(year = as.character(year))
-  nreg <- 0
-  suppressMessages({
-    for (i in 1:length(unique(pts$year))) {
-      pts_filtered_year <-
-        pts %>%
-        filter(year == sort(unique(pts$year))[i])
-
-      nreg2 <- lengths(st_intersects(map_geom, pts_filtered_year))
-      nreg <- nreg + nreg2
-      map_geom <- bind_cols(map_geom, nreg)
-    }
-  })
-
-  name_list <- sub(" ", "", paste("y", sort(unique(pts$year))))
-  colnames(map_geom)[6:(ncol(map_geom) - 1)] <- name_list
-
-  map_geom
-}
-map_nreg_along_years <- nreg_along_years(record_data, g025_geom[1:5])
+# Pre-process data) -----------------------------------------------------------
+map_nreg_along_years <- nreg.along.years(record_data, g025_geom[1:5])
 
 # Plot -----------------------------------------------------------------------
-plot_along_years <- function(myfill) {
-  map_nreg_along_years <-
-    map_nreg_along_years %>%
-    mutate(across(starts_with("y"), as.numeric))
-
-  nmax <-
-    st_drop_geometry(map_nreg_along_years) %>%
-    select({{ myfill }}) %>%
-    max()
-
-  ggplot(map_nreg_along_years) +
-    geom_sf(aes_string(fill = {{ myfill }}), size = 0.2) +
-    labs(fill = "Number of \n mammal records") +
-    scale_fill_viridis_b(
-      limits = c(0, nmax),
-      breaks = c(
-        0,
-        round(nmax / 6, 0),
-        round(2 * nmax / 6, 0),
-        round(3 * nmax / 6, 0),
-        round(4 * nmax / 6, 0),
-        round(5 * nmax / 6, 0),
-        nmax
-      )
-    ) +
-    theme_light() +
-    theme(
-      axis.text.x = element_text(angle = 45, hjust = 0.75),
-      legend.title = element_text(size = 10),
-      legend.text = element_text(size = 9)
-    ) +
-    ggtitle(paste(sub("y", "", myfill))) +
-    guides(
-      fill = guide_colorbar(
-        draw.ulim = FALSE,
-        draw.llim = FALSE
-      )
-    )
-}
-
 myfill_list <- colnames(map_nreg_along_years)[6:(ncol(map_nreg_along_years) - 1)]
-plot_list <- lapply(myfill_list, FUN = plot_along_years)
+
+plot_list <- lapply(myfill_list, FUN = plot.along.years)
 
 # Save video ----------------------------------------------------------------
 
 saveVideo(
   expr = print(plot_list),
-  video.name = "animation.mp4",
-  img.name = "Rplot",
+  video.name = "../data/results/animation-nreg-all-mammals.mp4",
+  img.name = "animation-nreg-all-mammals",
   ffmpeg = ani.options("ffmpeg"),
   other.opts='-pix_fmt yuv420p -b 1000k'
 )
 
-saveHTML(expr = {
-  png(ani.options("img.fmt"))
-  print(plot_list)
-  dev.off()
-}, img.name = "custom_plot", use.dev = FALSE, ani.type = "png", 
-description = "Note how we use our own graphics device in 'expr'.", 
-htmlfile = "custom_device.html")
+saveHTML(
+  expr = {
+    png(ani.options("img.fmt"))
+    print(plot_list)
+    dev.off()
+  },
+  img.name = "custom_plot",
+  use.dev = FALSE,
+  ani.type = "png",
+  htmlfile = "custom_device.html"
+)
---
+++ code/R-scripts/functions/06b-funs-animation-plot-map.R
@@ -0,0 +1,95 @@
+# File purpose: Functions to process data and plot maps to make animation
+# Date: 30/11/2020
+
+library(rlang)
+library(sf)
+library(ggplot2)
+library(dplyr)
+
+conflicted::conflict_prefer("filter", "dplyr")
+
+nreg.along.years <- function(pts, map_geom) {
+  # Create table with count of records along years
+  # 
+  # Args:
+  #   pts: sf point map with column "year"
+  #   map_gom: sf multipolygon map
+  
+  # Remove year with "NA" (in this case is "NA", not NA)
+  pts <-
+    pts %>%
+    filter(year != "NA") %>%
+    mutate(year = as.character(year))
+  
+  nreg <- 0
+  suppressMessages({
+    for (i in 1:length(unique(pts$year))) {
+      pts_filtered_year <-
+        pts %>%
+        filter(year == sort(unique(pts$year))[i])
+
+      nregy <- lengths(st_intersects(map_geom, pts_filtered_year))
+      
+      # Make it cumulative through years
+      nreg <- nreg + nregy
+      map_geom <- bind_cols(map_geom, nreg)
+    }
+  })
+
+  # Column names as number is very problematic, so add an letter (without space)
+  # Identify columns with counts for each year
+  name_list <- sub(" ", "", paste("y", sort(unique(pts$year))))
+  colnames(map_geom)[6:(ncol(map_geom) - 1)] <- name_list
+
+  map_geom
+}
+
+plot.along.years <- function(myfill) {
+  # Plot map for each year
+  # 
+  # Arg:
+  #   myfill: list containing the column names
+  
+  # The map wasn't add in the function input base apparently so far there is 
+  # no easy way to use apply to a sf object, so I added the map inside the 
+  # function instead of adding it in the function input
+  map_nreg_along_years <-
+    map_nreg_along_years %>%
+    mutate(across(starts_with("y"), as.numeric))
+
+  # Get maximum number of records to set break points later
+  nmax <-
+    st_drop_geometry(map_nreg_along_years) %>%
+    select({{ myfill }}) %>%
+    max()
+
+  # The {{ }} is a trick from rlang package
+  ggplot(map_nreg_along_years) +
+    geom_sf(aes_string(fill = {{ myfill }}), size = 0.2) +
+    labs(fill = "Number of \n mammal records") +
+    scale_fill_viridis_b(
+      limits = c(0, nmax),
+      breaks = c(
+        0,
+        round(nmax / 6, 0),
+        round(2 * nmax / 6, 0),
+        round(3 * nmax / 6, 0),
+        round(4 * nmax / 6, 0),
+        round(5 * nmax / 6, 0),
+        nmax
+      )
+    ) +
+    theme_light() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 0.75),
+      legend.title = element_text(size = 10),
+      legend.text = element_text(size = 9)
+    ) +
+    ggtitle(paste(sub("y", "", myfill))) +
+    guides(
+      fill = guide_colorbar(
+        draw.ulim = FALSE,
+        draw.llim = FALSE
+      )
+    )
+}
