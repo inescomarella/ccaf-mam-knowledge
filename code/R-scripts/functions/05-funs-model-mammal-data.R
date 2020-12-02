63a36f905c56af88c011bde69255c60b9f811cc6
InesComarella <inesmottacomarella@gmail.com>
2020-12-02 08:58:15 -0300

    :fire: Remove function to count species
    Not useful in the 05-do-model-mammal-data.R anymore after I removed the statistical analysis.

--- code/R-scripts/functions/05-funs-model-mammal-data.R
+++ code/R-scripts/functions/05-funs-model-mammal-data.R
@@ -4,28 +4,6 @@
 library(sf)
 library(FNN)
 
-count.sp.in.polygons <- function(pts, polygons) {
-  # Count number of species in each polygon
-  # Args:
-  #   pts: sf point object containing a "species" columns
-  #   polygons: sf multipolygon object
-  # Output:
-  #   polygons sf with an extra column called "nsp" containing the number
-  #   of species
-
-  nsp <- c()
-  suppressMessages({
-    for (i in 1:nrow(polygons)) {
-      polySelect <- polygons[i, ]
-      pts2 <- st_intersection(pts, polySelect)
-      nsp[i] <- length(unique(pts2$species))
-    }
-  })
-
-  cbind(polygons, nsp)
-}
-
-
 get.nearest.dist <- function(pts, polygon) {
   # Get distance from the centre point of each polygon in a multipolygon sf to
   # the nearest point in a point sf
