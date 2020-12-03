25b42c80050ebc23c5a7b9f23d61b93dc5b74979
InesComarella <inesmottacomarella@gmail.com>
2020-12-03 11:30:44 -0300

    :boom: Reorganize folder in Drive
    Results structured as the project, folder organized by project goals

--- code/R-scripts/07-do-upload-to-drive.R
+++ code/R-scripts/07-do-upload-to-drive.R
@@ -20,52 +20,21 @@ drive_auth(
 # Prepare folder and files ---------------------------------------------------
 
 # Check if folders already exist and create them
-if (nrow(drive_get(path = "~/ccma-tcc/results/correlation")) == 0) {
-  drive_mkdir("~/ccma-tcc/results/correlation")
+if (nrow(drive_get(path = "~/ccma-tcc/results/ccma-knowledge-hotspots")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/ccma-knowledge-hotspots")
 }
 
-if (nrow(drive_get(path = "~/ccma-tcc/results/model")) == 0) {
-  drive_mkdir("~/ccma-tcc/results/model")
+if (nrow(drive_get(path = "~/ccma-tcc/results/ccma-knowledge-hotspots/order-maps")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/ccma-knowledge-hotspots/order-maps")
 }
 
-if (nrow(drive_get(path = "~/ccma-tcc/results/order-maps")) == 0) {
-  drive_mkdir("~/ccma-tcc/results/order-maps")
+if (nrow(drive_get(path = "~/ccma-tcc/results/species-knowledge-gaps")) == 0) {
+  drive_mkdir("~/ccma-tcc/results/species-knowledge-gaps")
 }
 
-if (nrow(drive_get(path = "~/ccma-tcc/results/all-mammals-maps")) == 0) {
-  drive_mkdir("~/ccma-tcc/results/all-mammals-maps")
-}
 
 # Get files path to upload
-species_table_path <- "../data/results/species-table.xlsx"
-
-records_plot_path <- "../data/results/first-last-record-plot.pdf"
-
-file_paths_correlation <-
-  list.files(
-    "../data/results",
-    pattern = "correlation",
-    full.names = TRUE,
-    recursive = TRUE
-  )
-
-file_paths_model_plots <-
-  list.files(
-    "../data/results",
-    pattern = "model",
-    full.names = TRUE,
-    recursive = TRUE
-  )
-
-file_paths_orders <-
-  list.files(
-    "../data/results",
-    pattern = "order",
-    full.names = TRUE,
-    recursive = TRUE
-  )
-
-file_paths_all_mammals <-
+all_mammals_paths <-
   list.files(
     "../data/results",
     pattern = "all",
@@ -73,78 +42,68 @@ file_paths_all_mammals <-
     recursive = TRUE
   )
 
+orders_plot_paths <-
+  list.files(
+    "../data/results",
+    pattern = "order",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+first_last_rec_plot_path <-
+  "../data/results/first-last-record-plot.pdf"
+
+species_table_path <- "../data/results/species-table.xlsx"
+
+
 # Get file names by removing the path
-species_table_name <- str_replace(species_table_path, "../data/results/", "")
+all_mammals_names <-
+  str_replace(all_mammals_paths, "../data/results/", "")
 
-records_plot_name <- str_replace(records_plot_path, "../data/results/", "")
+orders_plot_names <-
+  str_replace(orders_plot_paths, "../data/results/", "")
 
-file_names_correlation <-
-  str_replace(file_paths_correlation, "../data/results/", "")
+first_last_rec_plot_name <-
+  str_replace(first_last_rec_plot_path, "../data/results/", "")
 
-file_names_model_plots <-
-  str_replace(file_paths_model_plots, "../data/results/", "")
-
-file_names_orders <-
-  str_replace(file_paths_orders, "../data/results/", "")
-
-file_names_all_mammals <-
-  str_replace(file_paths_all_mammals, "../data/results/", "")
+species_table_name <-
+  str_replace(species_table_path, "../data/results/", "")
 
 # Upload files ---------------------------------------------------------------
 
 # Upload all files to its respective folders
+for (i in 1:length(all_mammals_paths)) {
+  drive_upload(
+    all_mammals_paths[i],
+    name = all_mammals_names[i],
+    path = "~/ccma-tcc/results/ccma-knowledge-hotspots",
+    overwrite = TRUE,
+    verbose = TRUE
+  )
+}
+
+for (i in 1:length(orders_plot_names)) {
+  drive_upload(
+    orders_plot_paths[i],
+    name = orders_plot_names[i],
+    path = "~/ccma-tcc/results/ccma-knowledge-hotspots/order-maps",
+    overwrite = TRUE,
+    verbose = TRUE
+  )
+}
+
+drive_upload(
+  first_last_rec_plot_path,
+  name = first_last_rec_plot_name,
+  path = "~/ccma-tcc/results/species-knowledge-gaps",
+  overwrite = TRUE,
+  verbose = TRUE
+)
+
 drive_upload(
   species_table_path,
   name = species_table_name,
-  path = "~/ccma-tcc/results",
+  path = "~/ccma-tcc/results/species-knowledge-gaps",
   overwrite = TRUE,
   verbose = TRUE
 )
-
-drive_upload(
-  records_plot_path,
-  name = records_plot_name,
-  path = "~/ccma-tcc/results",
-  overwrite = TRUE,
-  verbose = TRUE
-)
-
-for (i in 1:length(file_paths_correlation)) {
-  drive_upload(
-    file_paths_correlation[i],
-    name = file_names_correlation[i],
-    path = "~/ccma-tcc/results/correlation",
-    overwrite = TRUE,
-    verbose = TRUE
-  )
-}
-
-for (i in 1:length(file_paths_model_plots)) {
-  drive_upload(
-    file_paths_model_plots[i],
-    name = file_names_model_plots[i],
-    path = "~/ccma-tcc/results/model",
-    overwrite = TRUE,
-    verbose = TRUE
-  )
-}
-
-for (i in 1:length(file_paths_all_mammals)) {
-  drive_upload(
-    file_paths_all_mammals[i],
-    name = file_names_all_mammals[i],
-    path = "~/ccma-tcc/results/all-mammals-maps",
-    overwrite = TRUE,
-    verbose = TRUE
-  )
-}
-
-for (i in 1:length(file_paths_orders)) {
-  drive_upload(
-    file_paths_orders[i],
-    name = file_names_orders[i],
-    path = "~/ccma-tcc/results/order-maps",
-    overwrite = TRUE,
-    verbose = TRUE
-  )
-}
