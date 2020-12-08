ee48b3b4a43258d7f8be7e78381e2603da22e0b9
InesComarella <inesmottacomarella@gmail.com>
2020-12-08 14:40:00 -0300

    :sparkles: Make grid in R
    Rather then read the grid made in QGis, just read the original file and modify it

--- code/R-scripts/05-do-scatter-plot.R
+++ code/R-scripts/05-do-scatter-plot.R
@@ -4,7 +4,7 @@
 # Load in libraries
 library(tidyverse)
 library(sf)
-library(sp)
+library(brazilmaps)
 
 conflicted::conflict_prefer(name = "filter", winner = "dplyr")
 conflicted::conflict_prefer(name = "select", winner = "dplyr")
@@ -12,11 +12,17 @@ conflicted::conflict_prefer(name = "select", winner = "dplyr")
 # Source functions
 source("./R-scripts/functions/05-funs-scatter-plot.R")
 
+# Projections
+utm <-
+  sp::CRS("+proj=utm +zone=24 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+longlat <- sp::CRS("+proj=longlat +datum=WGS84")
+
 # Load data -------------------------------------------------------------------
 record_pts <-
   st_read(
     dsn = "../data/processed-data/clean-mammal-data.csv",
-    crs = CRS("+proj=longlat +datum=WGS84"),
+    crs = longlat,
     options = c(
       "X_POSSIBLE_NAMES=decimalLongitude",
       "Y_POSSIBLE_NAMES=decimalLatitude"
@@ -26,7 +32,7 @@ record_pts <-
 institute_pts <-
   st_read(
     dsn = "../data/raw-data/research-institutes.csv",
-    crs = CRS("+proj=longlat +datum=WGS84"),
+    crs = longlat,
     options = c(
       "X_POSSIBLE_NAMES=longitude",
       "Y_POSSIBLE_NAMES=latitude"
@@ -36,35 +42,57 @@ institute_pts <-
 cus_geom <-
   st_read(dsn = "../data/processed-data/", layer = "CUs-map")
 
-g025_geom <-
-  st_read(dsn = "../data/processed-data/", layer = "grid-025-clipped")
+corridors <- st_read(
+  dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
+  layer = "corredores_ppg7",
+  check_ring_dir = TRUE
+)
+
+# Pre-process map ------------------------------------------------------------
+
+# Keep only CCAF
+ccaf_longlat <- 
+  corridors %>% 
+  filter(str_detect(NOME1, "Mata")) %>%
+  st_set_crs(longlat)
+
+# Get Brazil map
+br_longlat <-
+  get_brmap(geo = "Brazil")%>% 
+  st_as_sf() %>%
+  st_transform(longlat)
+
+# Clip, make grid and convert to metric coordinate system
+ccaf_grid <- 
+  st_intersection(ccaf_longlat, br_longlat) %>%
+  st_make_grid(square = FALSE, cellsize = 0.3) %>%
+  st_transform(utm) %>%
+  st_as_sf()
+
+# Check cellsize
+ccaf_grid %>% 
+  st_area() %>% 
+  mean()
 
 # Process data ----------------------------------------------------------------
 
 # Reproject to a metric coordinate system
-utm <-
-  CRS("+proj=utm +zone=24 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
-
 rcrd_utm <- st_transform(record_pts, utm)
 inst_utm <- st_transform(institute_pts, utm)
 cus_utm <- st_set_crs(cus_geom, utm)
-g025_utm <- st_transform(g025_geom, utm)
-
-# Measure mean polygon area
-g025_area <- mean(st_area(g025_utm)) # 484.516.066 m² = 484 km²
 
 # Measure the distance of each cell grid to the nearest research institute
-g025_utm <- get.nearest.dist(inst_utm, g025_utm)
+ccaf_grid <- get.nearest.dist(inst_utm, ccaf_grid)
 
 # Count records per cell
-g025_utm$nrec <- lengths(st_intersects(g025_utm, rcrd_utm))
+ccaf_grid$nrec <- lengths(st_intersects(ccaf_grid, rcrd_utm))
 
 # Get CU presence in each cell
-g025_utm$CU <- lengths(st_intersects(g025_utm, cus_utm))
+ccaf_grid$CU <- lengths(st_intersects(ccaf_grid, cus_utm))
 
 # Save plots -----------------------------------------------------------------
 scatter_plot <-
-  st_drop_geometry(g025_utm) %>%
+  st_drop_geometry(ccaf_grid) %>%
   mutate(CU = ifelse(CU == 1, "Present", "Absent")) %>%
   mutate(dist_inst = dist_inst / 1000) %>%
   ggplot() +
