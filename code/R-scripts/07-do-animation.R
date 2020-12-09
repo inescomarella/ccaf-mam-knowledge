a895159ff066f1eb140e0c201d08c6b2b4dc48a5
InesComarella <inesmottacomarella@gmail.com>
2020-12-09 09:32:10 -0300

    :fire: Remove video and html
    GIF has a better resolution

--- code/R-scripts/07-do-animation.R
+++ code/R-scripts/07-do-animation.R
@@ -7,7 +7,6 @@ library(animation)
 library(sf)
 library(brazilmaps)
 
-
 conflicted::conflict_prefer("filter", "dplyr")
 
 # Source functions
@@ -37,7 +36,7 @@ longlat <- sp::CRS("+proj=longlat +datum=WGS84")
 
 # Get Brazil map
 br_longlat <-
-  get_brmap(geo = "Brazil")%>% 
+  get_brmap(geo = "Brazil") %>% 
   st_as_sf() %>%
   st_transform(longlat)
 
@@ -50,29 +49,32 @@ ccaf_longlat <-
 ccaf_clipped <-
   st_intersection(ccaf_longlat, br_longlat)
 
-# Clip, make grid and convert to metric coordinate system
+# Clip CCAF, make grid and convert to metric coordinate system
 # Ignore the warnings
-
 ccaf_grid <- 
   ccaf_clipped %>%
   st_make_grid(square = FALSE, cellsize = 0.3) %>%
   st_intersection(ccaf_longlat, br_longlat) %>%
   st_as_sf()
 
+# Clip grid
 ccaf_grid <-
   st_intersection(ccaf_grid, ccaf_clipped)
 
 # Pre-process data -----------------------------------------
 
 # Create the table with counts of records along years
-map_nrec_along_years <- nrec.along.years(record_data, ccaf_grid)
+map_nrec_along_years <- 
+  nrec.along.years(record_data, ccaf_grid)
 
-map_nsp_along_years <- nsp.along.years(record_data, ccaf_grid)
+map_nsp_along_years <- 
+  nsp.along.years(record_data, ccaf_grid)
 
 # Plot -----------------------------------------------------
 
 # Get columns names
-myfill_list <- colnames(map_nrec_along_years)[3:(ncol(map_nrec_along_years) - 1)]
+myfill_list <- 
+  colnames(map_nrec_along_years)[3:(ncol(map_nrec_along_years) - 1)]
 
 # Create plot list
 # Important! The map_nrec_along_years is read inside the function, so pay
@@ -87,50 +89,25 @@ together_plot_list <-
   lapply(myfill_list, FUN = plot.together.years)
 
 # Save video -----------------------------------------------
+# Save in gif format
+# The resolution is better than mp4
 saveGIF(
-  print(together_plot_list),
-  movie.name = "../animation_together.gif",
+  print(nrec_plot_list),
+  movie.name = "animation_nrec.gif",
   img.name = "Rplot",
   convert = "magick"
 )
 
 saveGIF(
   print(nsp_plot_list),
-  movie.name = "../animation_nsp.gif",
+  movie.name = "animation_nsp.gif",
   img.name = "Rplot",
   convert = "magick"
 )
 
-# Save in gif format
-# The resolution is better than mp4
 saveGIF(
-  print(nrec_plot_list),
-  movie.name = "../animation.gif",
+  print(together_plot_list),
+  movie.name = "animation_together.gif",
   img.name = "Rplot",
   convert = "magick"
-)
-
-# Save in mp4 format
-# Unfortunately the resolution is not very good :c
-saveVideo(
-  expr = print(plot_list),
-  video.name = "../data/results/animation-nrec-all-mammals.mp4",
-  img.name = "animation-nrec-all-mammals",
-  ffmpeg = ani.options("ffmpeg"),
-  other.opts = "-pix_fmt yuv420p -b 1000k"
-)
-
-
-# Save in html
-# I got an error when trying to change de directory
-saveHTML(
-  expr = {
-    png(ani.options("img.fmt"))
-    print(plot_list)
-    dev.off()
-  },
-  img.name = "custom_plot",
-  use.dev = FALSE,
-  ani.type = "png",
-  htmlfile = "custom_device.html"
 )
