5026b4c46ffdc08618c011db8965d2ab0cefe5cf
InesComarella <inesmottacomarella@gmail.com>
2020-12-13 14:20:59 -0300

    :art: Organize code, add comments
    Standardize and structure code, and save plot

--- code/R-scripts/test-raster.R
+++ code/R-scripts/test-raster.R
@@ -1,54 +1,91 @@
+# File purpose: Make CCAF first map
+# Date: 13/12/2020
+
+# Load libraries
 library(tidyverse)
 library(sf)
 library(raster)
+library(brazilmaps)
+library(ggspatial)
+library(rworldmap)
+library(cowplot)
 
+conflicted::conflict_prefer(name = "filter", winner = "dplyr")
+conflicted::conflict_prefer(name = "select", winner = "dplyr")
 
-# Projections
+# Set projection
 longlat <- sp::CRS("+proj=longlat +datum=WGS84")
 
-land_use_table <-
+# Load data ---------------------------------------------------
+
+br_sf <-
+  get_brmap(geo = "Brazil") %>%
+  st_as_sf() %>%
+  st_transform(longlat)
+
+ccaf_spatial <-
+  st_read(
+    dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
+    layer = "corredores_ppg7",
+    check_ring_dir = TRUE
+  ) %>%
+  st_set_crs(longlat) %>%
+  # Only Central Corridor of Atlantic Forest
+  filter(str_detect(NOME1, "Mata")) %>%
+  # Only terrestrial area
+  st_intersection(br_sf) %>%
+  # Fix name
+  mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica") %>%
+  as(., "Spatial")
+
+land_use_spdf <-
+  raster(
+    "../data/raw-data/maps/mapbiomas/mapbiomas-brazil-collection-50-mataatlantica-2019.tif"
+  ) %>%
+  crop(ccaf_spatial) %>%
+  mask(ccaf_spatial) %>%
+  as(., "SpatialPixelsDataFrame") %>%
+  as.data.frame()
+
+land_use_table_df <-
   read.csv(
     "../data/raw-data/maps/mapbiomas/mapbiomas-brazil-collection-50-mataatlantica-area.csv"
   )
 
-land_use_af_mapbiomas <-
-  raster(
-    "../data/raw-data/maps/mapbiomas/mapbiomas-brazil-collection-50-mataatlantica-2019.tif"
+cus_sf <-
+  st_read(dsn = "../data/processed-data/", layer = "CUs-map") %>%
+  st_transform(longlat) %>%
+  st_make_valid() %>%
+  st_intersection(st_as_sf(ccaf_spatial))
+
+institute_sf <-
+  st_read(
+    dsn = "../data/raw-data/research-institutes.csv",
+    crs = longlat,
+    options = c(
+      "X_POSSIBLE_NAMES=longitude",
+      "Y_POSSIBLE_NAMES=latitude"
+    )
   )
 
-corridors <- st_read(
-  dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
-  layer = "corredores_ppg7",
-  check_ring_dir = TRUE
-) %>%
-  filter(str_detect(NOME1, "Mata")) %>%
+ccaf_all_area_sf <-
+  st_read(
+    dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
+    layer = "corredores_ppg7",
+    check_ring_dir = TRUE
+  ) %>%
   st_set_crs(longlat) %>%
-  mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica")
+  # Only Central Corridor of Atlantic Forest
+  filter(str_detect(NOME1, "Mata")) %>%
+  # Fix name
+  mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica") %>%
+  st_as_sf()
 
-br_longlat <-
-  brazilmaps::get_brmap(geo = "Brazil") %>%
-  st_as_sf() %>%
-  st_transform(longlat)
+# Pre-process data --------------------------------------------
 
-corridors <-
-  st_intersection(corridors, br_longlat)
-
-corridors <- as(corridors, "Spatial")
-
-land_use_af_mapbiomas <- crop(land_use_af_mapbiomas, corridors)
-land_use_af_mapbiomas <- mask(land_use_af_mapbiomas, corridors)
-
-land_use_af_mapbiomas <-
-  as(land_use_af_mapbiomas, "SpatialPixelsDataFrame")
-
-land_use_af_mapbiomas <- as.data.frame(land_use_af_mapbiomas)
-
-colnames(land_use_af_mapbiomas) <- c("value", "x", "y")
-
-land_use_table <-
-  land_use_table %>%
-  mutate(id = ifelse(
-    class == 0,
+land_use_table_df <-
+  land_use_table_df %>%
+  mutate(id = ifelse(class == 0,
     "Non observed",
     ifelse(
       class < 10,
@@ -72,7 +109,7 @@ land_use_table <-
     )
   )) %>%
   mutate(value = class) %>%
-  dplyr::select(value, id, class_name)
+  select(value, id, class_name)
 
 color_table <-
   tibble(
@@ -85,82 +122,93 @@ color_table <-
       "Non observed"
     ),
     color = c(
-      "#06bd00", #dark green
-      "#2bff00", #light green
-      "#ffe100", #yellow
-      "#ff00cc", #magenta
-      "#0000FF", #blue
-      "#a6a6a6" #grey
+      # dark green
+      "#06bd00",
+      # light green
+      "#2bff00",
+      # yellow
+      "#ffe100",
+      # magenta
+      "#ff00cc",
+      # blue
+      "#0000FF",
+      # grey
+      "#a6a6a6"
     )
   )
 
+colnames(land_use_spdf) <- c("value", "x", "y")
 
-land_use_af_mapbiomas <-
-  merge(land_use_af_mapbiomas, land_use_table)
+land_use_spdf <-
+  merge(land_use_spdf, land_use_table_df)
 
-cus_geom <-
-  st_read(dsn = "../data/processed-data/", layer = "CUs-map") %>%
-  st_transform(sp::CRS("+proj=longlat +datum=WGS84"))
-
-institute_pts <-
-  st_read(
-    dsn = "../data/raw-data/research-institutes.csv",
-    crs = longlat,
-    options = c(
-      "X_POSSIBLE_NAMES=longitude",
-      "Y_POSSIBLE_NAMES=latitude"
+land_use_spdf$id <-
+  factor(
+    land_use_spdf$id,
+    levels = c(
+      "Forest",
+      "Non Forest Natural Formation",
+      "Farming",
+      "Non vegetated area",
+      "Water",
+      "Non observed"
     )
   )
-
-library(ggspatial)
-
-
-ccaf_longlat <-
-  st_read(
-    dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
-    layer = "corredores_ppg7",
-    check_ring_dir = TRUE
-  ) %>%
-  filter(str_detect(NOME1, "Mata")) %>%
-  st_set_crs(longlat) %>%
-  mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica")
 
 brazil_cropped <-
-  brazilmaps::get_brmap(geo = "State") %>%
+  get_brmap(geo = "State") %>%
   st_as_sf() %>%
   st_transform(longlat) %>%
-  st_crop(st_bbox(ccaf_longlat))
+  st_crop(st_bbox(ccaf_all_area_sf))
 
-ccaf_longlat <- st_difference(ccaf_longlat, st_as_sf(corridors))
+ccaf_marine_area_sf <-
+  st_difference(ccaf_all_area_sf, st_as_sf(ccaf_spatial))
 
-land_use_af_mapbiomas$id <- factor(land_use_af_mapbiomas$id, levels = c("Forest", "Non Forest Natural Formation", "Farming", "Non vegetated area", "Water", "Non observed"))
+south_america_sf <-
+  getMap()[-which(getMap()$ADMIN == "Antarctica"), ] %>%
+  spTransform(longlat) %>%
+  st_as_sf() %>%
+  filter(Stern == "South America")
 
+BA_ES_sf <-
+  get_brmap(geo = "State") %>%
+  st_as_sf() %>%
+  st_transform(longlat) %>%
+  filter(State == "32" | State == "29")
 
+brazil_states_sf <-
+  get_brmap(geo = "State") %>%
+  st_as_sf() %>%
+  st_transform(longlat)
 
-ccaf_blah <-
-  st_read(
-    dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
-    layer = "corredores_ppg7",
-    check_ring_dir = TRUE
-  ) %>%
-  filter(str_detect(NOME1, "Mata")) %>%
-  st_set_crs(longlat) %>%
-  mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica")
+# Plot maps ---------------------------------------------------
 
 zoom_in_map <-
   ggplot() +
   geom_sf(data = brazil_cropped) +
-  geom_raster(data = land_use_af_mapbiomas, aes(x = x, y = y, fill = id)) +
+  geom_raster(data = land_use_spdf, aes(x = x, y = y, fill = id)) +
   scale_fill_manual(values = color_table$color) +
-  geom_sf(data = st_as_sf(corridors), fill = NA, size = 0.2) +
-  geom_sf(data = ccaf_longlat, fill = "#0000FF", size = 0.2) +
-  geom_sf(data = cus_geom, fill = NA, size = 0.3, color = "black") +
-  geom_sf(data = institute_pts, size = 0.7, color = "white", pch = 17) +
+  geom_sf(
+    data = ccaf_all_area_sf,
+    fill = NA,
+    size = 0.2
+  ) +
+  geom_sf(data = ccaf_marine_area_sf, fill = "#0000FF", size = 0.2) +
+  geom_sf(
+    data = cus_sf,
+    fill = NA,
+    size = 0.3,
+    color = "black"
+  ) +
+  geom_sf(
+    data = institute_sf,
+    size = 0.7,
+    color = "white",
+    pch = 17
+  ) +
   theme_light() +
-  
   # Scale bar in the bottom right
   annotation_scale(location = "br", width_hint = 0.2) +
-  
   # North arrow in the bottom right above scale bar
   annotation_north_arrow(
     location = "br",
@@ -169,7 +217,7 @@ zoom_in_map <-
     width = unit(1.5, "cm"),
     pad_y = unit(0.5, "in")
   ) +
-  cowplot::background_grid("none") +
+  background_grid("none") +
   theme(
     axis.text.y = element_text(angle = 90, hjust = 0.3),
     axis.title = element_blank(),
@@ -178,38 +226,24 @@ zoom_in_map <-
     axis.text = element_text(size = 10),
     legend.key.size = unit(0.7, "cm")
   ) +
-  coord_sf(
+  coord_sf( 
     # Plot axis y in the right
     label_graticule = "SE"
   )
-
-
-library(rworldmap)
-south_america_sf <- getMap()[-which(getMap()$ADMIN == "Antarctica"), ] %>%
-  spTransform(longlat) %>%
-  st_as_sf() %>%
-  filter(Stern == "South America")
-
-BA_ES_sf <-
-  brazilmaps::get_brmap(geo = "State") %>%
-  st_as_sf() %>%
-  st_transform(longlat) %>%
-  filter(State == "32" | State == "29")
-
-brazil_states_sf <-
-  brazilmaps::get_brmap(geo = "State") %>%
-  st_as_sf() %>%
-  st_transform(longlat)
 
 zoom_out_map <-
   ggplot() +
   geom_sf(data = south_america_sf, fill = NA, size = 0.2) +
   geom_sf(data = brazil_states_sf, size = 0.2) +
-  geom_sf(data = st_as_sf(corridors), fill = 'red', size = 0.2) +
-  geom_sf(data = ccaf_longlat, fill = "red", size = 0.2) +
-  geom_sf(data = BA_ES_sf, fill = NA, size = 0.4) +   
+  geom_sf(
+    data = st_as_sf(ccaf_spatial),
+    fill = "red",
+    size = 0.2
+  ) +
+  geom_sf(data = ccaf_all_area_sf, fill = "red", size = 0.2) +
+  geom_sf(data = BA_ES_sf, fill = NA, size = 0.4) +
   geom_rect(
-    data = ccaf_longlat,
+    data = ccaf_all_area_sf,
     xmin = -41.87851,
     xmax = -37.27757,
     ymin = -21.30999,
@@ -218,12 +252,21 @@ zoom_out_map <-
     size = 0.2,
     colour = "black"
   ) +
-  cowplot::background_grid("none") +
-  cowplot::theme_nothing()
+  background_grid("none") +
+  theme_nothing()
 
-library(cowplot)
-cowplot::ggdraw() +
-  cowplot::draw_plot(zoom_in_map, hjust = -0.1) +
-  cowplot::draw_plot(zoom_out_map, scale = 0.5, hjust = 0.38, vjust = -0.25)
+final_plot <-
+  ggdraw() +
+  draw_plot(zoom_in_map, hjust = -0.1) +
+  draw_plot(zoom_out_map,
+    scale = 0.5,
+    hjust = 0.38,
+    vjust = -0.25
+  )
 
-rm(arrowB)  
+# Save map ---------------------------------------------------
+save_plot("../data/results/ccaf-map.pdf",
+  final_plot,
+  base_height = 6,
+  base_width = 8
+)
