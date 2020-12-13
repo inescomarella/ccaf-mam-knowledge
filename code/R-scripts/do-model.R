60244435b54280251c08f9f7fe26a14f0297efb3
InesComarella <inesmottacomarella@gmail.com>
2020-12-13 10:15:35 -0300

    :art: Organize code, add comments
    Standardized code, more clear and clean

--- code/R-scripts/do-model.R
+++ code/R-scripts/do-model.R
@@ -1,20 +1,23 @@
 # File purpose: Do data analysis, run models, correlation test
 # Data: 17/11/2020
+# 
+#########################################################################
+# For details on overdispersion test check:
+# > Overdispersion, and how to deal with it in R and JAGS
+# > DHARMa: residual diagnostics for hierarchical (multi-level/mixed)
+# regression models
+#########################################################################
 
 # Load in libraries
 x <-
   c(
     "tidyverse",
-    "raster",
     "sf",
-    "rgdal",
     "openxlsx",
-    "FNN",
     "vegan",
-    "corrplot",
     "DHARMa",
     "MASS",
-    "MuMIn"
+    "brazilmaps"
   )
 lapply(x, library, character.only = TRUE)
 
@@ -24,13 +27,13 @@ conflicted::conflict_prefer(name = "select", winner = "dplyr")
 # Source functions
 source("./R-scripts/functions/05-funs-scatter-plot.R")
 
+# Set projections
+longlat <- sp::CRS("+proj=longlat +datum=WGS84")
 utm <-
   sp::CRS("+proj=utm +zone=24 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
-longlat <- sp::CRS("+proj=longlat +datum=WGS84")
-
-# Load in data
-record_pts_utm <-
+# Load data -------------------------------------------------------------
+records_utm <-
   st_read(
     dsn = "../data/processed-data/clean-mammal-data.csv",
     crs = longlat,
@@ -41,7 +44,7 @@ record_pts_utm <-
   ) %>%
   st_transform(utm)
 
-institute_pts_utm <-
+institutes_utm <-
   st_read(
     dsn = "../data/raw-data/research-institutes.csv",
     crs = longlat,
@@ -52,50 +55,45 @@ institute_pts_utm <-
   ) %>%
   st_transform(utm)
 
-# Get Brazil map
 br_longlat <-
-  brazilmaps::get_brmap(geo = "Brazil")%>% 
+  get_brmap(geo = "Brazil") %>%
   st_as_sf() %>%
   st_transform(longlat)
 
-ccaf_utm <- st_read(
+ccaf_longlat <- 
+  st_read(
   dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
   layer = "corredores_ppg7",
   check_ring_dir = TRUE
-) %>% 
-  filter(str_detect(NOME1, "Mata")) %>%
+  ) %>%
   st_set_crs(longlat) %>%
-  st_intersection(br_longlat) %>%
-  mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica") %>%
-  st_transform(utm)
-
-ccaf_longlat <- st_read(
-  dsn = "../data/raw-data/maps/MMA/corredores_ppg7",
-  layer = "corredores_ppg7",
-  check_ring_dir = TRUE
-) %>% 
+  # Only Central Corridor of Atlantic Forest
   filter(str_detect(NOME1, "Mata")) %>%
-  st_set_crs(longlat) %>%
+  # Keep only terrestrial area
   st_intersection(br_longlat) %>%
+  # Fix name
   mutate(NOME1 = "Corredor Ecologico Central da Mata Atlantica")
 
-cus_longlat <-
+cus_utm <-
   st_read(dsn = "../data/processed-data/", layer = "CUs-map") %>%
   st_transform(longlat) %>%
   st_make_valid() %>%
-  st_intersection(ccaf_longlat)
-
-cus_utm <-
-  cus_longlat %>%
+  st_intersection(ccaf_longlat) %>%
   st_transform(utm)
 
-# Pre-process map -----------------------------------------------------
+# Make grid -------------------------------------------------------------
 
-# expected area = 100000m * 10000m = 1e+9 m2 = 1000 km2
+# Reproject CCAF map to a metric coordinate system
+ccaf_utm <- 
+  ccaf_longlat %>%
+  st_transform(utm)
+
+# Expected area = 1000m * 1000000m = 1e+9 m2 = 1000 km2
 cellarea <- 1000 * (1e+6)
-cellsize <- 2 * sqrt(cellarea/((3*sqrt(3)/2))) * sqrt(3)/2
+cellsize <- 2 * sqrt(cellarea / ((3 * sqrt(3) / 2))) * sqrt(3) / 2
 
-ccaf_grid_utm <- 
+# Make the hexagon grid with the expected area as simple feature
+ccaf_grid_utm <-
   ccaf_utm %>%
   st_make_grid(cellsize = cellsize, square = FALSE) %>%
   st_as_sf()
@@ -105,39 +103,33 @@ a <- st_area(ccaf_grid_utm)[1]
 units(a) <- "km^2"
 a
 
-ccaf_grid_longlat <- 
-  ccaf_grid_utm %>%
-  st_transform(longlat)
+# Pre-process data ------------------------------------------------------
 
+# Check CU presence in the grid cell (presence/absence = 1/NA)
 ccaf_grid_utm$CU_presence <-
   ccaf_grid_utm %>%
-  st_intersects(st_buffer(cus_utm, 0)) %>% 
-  as.numeric() %>% 
+  st_intersects(st_buffer(cus_utm, 0)) %>%
+  as.numeric() %>%
   tibble()
 
+# Convert to presence/absence = TRUE/FALSE
 ccaf_grid_utm <-
   ccaf_grid_utm %>%
   mutate(CU_presence = !is.na(CU_presence))
 
-# Pre-process data -----------------------------------------------------------
-
-# Measure the distance of each cell grid to the nearest research institute
-ccaf_grid_utm <- get.nearest.dist(institute_pts_utm, ccaf_grid_utm)
+# Measure the distance from each cell grid centre to the nearest research institute
+ccaf_grid_utm <- get.nearest.dist(institutes_utm, ccaf_grid_utm)
 
 # Count records per cell
-ccaf_grid_utm$nreg <- lengths(st_intersects(ccaf_grid_utm, record_pts_utm))
+ccaf_grid_utm$nreg <-
+  lengths(st_intersects(ccaf_grid_utm, records_utm))
 
 # Extract dataframe
-ccaf_grid_df <- 
+ccaf_grid_df <-
   st_drop_geometry(ccaf_grid_utm) %>%
   select(CU_presence, dist_inst, nreg)
 
-# Correlation test -----------------------------------------------------------
-
-grid_cor <-
-  cor(ccaf_grid_df, method = c("pearson", "kendall", "spearman"))
-
-# Run models -----------------------------------------------------------------
+# Run models ------------------------------------------------------------
 
 # Fit Negative Binomial Generalized Linear Model
 summary(
@@ -149,12 +141,8 @@ summary(
     )
 )
 
-# Rank by AIC
-# Select best model to rerun the GLM
-glm_ranked <- dredge(glm_fitted)
-
-# Test quality of fit --------------------------------------------------------
-par(mar = c(1,1,1,1))
+# Test quality of fit ---------------------------------------------------
+par(mar = c(1, 1, 1, 1))
 
 # Conventional Residuals (fittedModel)
 glm_simulation <- simulateResiduals(glm_fitted, plot = T)
@@ -169,28 +157,7 @@ testDispersion(glm_simulation, alternative = "greater")
 # Test if there are more zeros than expected
 testZeroInflation(glm_simulation, alternative = "greater")
 
-# For details on overdispersion test check:
-# > Overdispersion, and how to deal with it in R and JAGS
-# > DHARMa: residual diagnostics for hierarchical (multi-level/mixed)
-# regression models
-
-# Save -----------------------------------------------------------------------
-
-# Correlation test plot
-pdf(
-  file = "../data/results/correlation-plot-grid.pdf",
-  width = 5,
-  height = 6
-)
-corrplot(
-  grid_cor,
-  type = "upper",
-  method = "circle",
-  tl.col = "black",
-  tl.srt = 45,
-  order = "original"
-)
-dev.off()
+# Save ------------------------------------------------------------------
 
 # Overdispersion plots
 pdf(
@@ -230,24 +197,11 @@ dev.off()
 # Model output
 glm_fitted_coef <-
   as.data.frame(coef(summary(glm_fitted))) %>%
-  mutate_if(is.numeric, ~ round(., 3))
-
-glm_ranked_df <-
-  as.data.frame(glm_ranked) %>%
-  mutate_if(is.numeric, ~ round(., 3))
-
-glm_refitted_coef <-
-  as.data.frame(coef(summary(glm_fitted))) %>%
-  mutate_if(is.numeric, ~ round(., 3))
+  mutate(variable = row.names(.)) %>%
+  mutate_if(is.numeric, ~ round(., 3)) %>%
+  relocate(variable)
 
 OUT <- createWorkbook()
-
-addWorksheet(OUT, "global-model-coef-grid")
-addWorksheet(OUT, "model-selection-grid")
-addWorksheet(OUT, "selected-model-coef-grid")
-
-writeData(OUT, sheet = "global-model-coef-grid", x = glm_fitted_coef)
-writeData(OUT, sheet = "model-selection-grid", x = glm_ranked_df)
-writeData(OUT, sheet = "selected-model-coef-grid", x = glm_refitted_coef)
-
+addWorksheet(OUT, "Sheet1")
+writeData(OUT, sheet = "Sheet1", x = glm_fitted_coef)
 saveWorkbook(OUT, "../data/results/model-results.xlsx", overwrite = TRUE)
