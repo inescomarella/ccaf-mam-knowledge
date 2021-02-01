51c12bd55b54e4ecbde412dd4574257fbbf2ab74
InesComarella <inesmottacomarella@gmail.com>
2021-02-01 09:37:44 -0300

    :art: Improve code, use Quintela et al 2020 table
    Take Quintela appendix from Scielo page and use species conservation status. Improve code to make plot and dataframe for first/last records.

--- code/R-scripts/06-do-explore-data.R
+++ code/R-scripts/06-do-explore-data.R
@@ -3,8 +3,7 @@
 # Data: 17/11/2020
 
 # Load in libraries
-library(tidyverse)
-library(openxlsx)
+xfun::pkg_attach2(c("httr", "rvest", "openxlsx", "tidyverse", "stringi", "openxlsx"))
 
 conflicted::conflict_prefer(name = "filter", winner = "dplyr")
 conflicted::conflict_prefer(name = "select", winner = "dplyr")
@@ -13,28 +12,104 @@ conflicted::conflict_prefer(name = "arrange", winner = "dplyr")
 # Source functions
 source("./R-scripts/functions/06-funs-explore-data.R")
 
-# Load data --------------------------------------------------
+# Load data -------------------------------------------------------------------
 
-data_read <- read.csv("../data/processed-data/clean-mammal-data.csv")
-cons_status <- read.csv("../data/raw-data/conservation-status.csv")
+data_read <-
+  read.csv("../data/processed-data/clean-mammal-data.csv") %>%
+  select(-X)
 
-# Pre-process data -------------------------------------------
+# Get species conservation status and endemism --------------------------------
+# From Quintela et al. 2020
+
+# Get the source code of the page
+scielo_page <- GET("https://www.scielo.br/scielo.php?pid=S0001-37652020000400706&script=sci_arttext")
+
+# Translate the website code
+parsed_scielo_page <- content(scielo_page)
+
+# Get tables from page
+scielo_tables <- html_table(parsed_scielo_page, fill = TRUE)
+
+# Appendix
+mammal_table <- scielo_tables[[3]]
+
+colnames(mammal_table) <- c("species", "IUCN", "ICMBio")
+
+mammal_table_clean <-
+  mammal_table[-1, ] %>% filter(IUCN != "" | ICMBio != "")
+
+mammal_information <-
+  mammal_table_clean %>%
+  mutate(
+    endemic = ifelse(
+      test = endsWith(species, " *"),
+      yes = "Yes",
+      no = "No"
+    ),
+    species = gsub("\\s*\\([^\\)]+\\)", "", as.character(species)),
+    species = gsub("[*]", "", as.character(species)),
+    species = gsub("  ", " ", as.character(species)),
+    species = stri_trim_both(species),
+    species = word(species, 1, 2),
+    species = ifelse(
+      str_detect(species, "hypoxanthus"),
+      "Brachyteles hypoxanthus",
+      ifelse(str_detect(species, "us geoffroyi"), "Leopardus geoffroyi",
+        ifelse(
+          str_detect(species, "\teucippe"),
+          "Mico leucippe",
+          ifelse(
+            str_detect(species, "ri sciureus"),
+            "Saimiri sciureus",
+            ifelse(
+              str_detect(species, "Echimys chr\n"),
+              "Echimys chrysurus",
+              ifelse(
+                str_detect(species, "\taurispinosus"),
+                "Nyctinomops aurispinosus",
+                ifelse(
+                  str_detect(species, "rrhinus angustirostris"),
+                  "Platyrrhinus angustirostris",
+                  species
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  ) %>%
+  # Following Reis et al., 2017
+  mutate(species = ifelse(
+    str_detect(species, " cinereus"),
+    "Dermanura cinerea",
+    species
+  )) %>%
+  # Following Reis et al., 2017
+  mutate(species = ifelse(
+    str_detect(species, " gnomus"),
+    "Dermanura gnoma",
+    species
+  ))
+
+# Pre-process data ------------------------------------------------------------
 
 # Add species conservation status
-data <- merge(data_read, cons_status, by = "species", all = TRUE)
+data <- merge(data_read, mammal_information, by = "species", all = TRUE)
 
+# Remove species not detected
 to_remove <- anti_join(select(data, colnames(data_read)), data_read)
 data <- anti_join(data, to_remove)
 
-# Tables -----------------------------------------------------
+# Tables ----------------------------------------------------------------------
 
 # List of species
-species_list <- 
+species_list <-
   data %>%
   select(order, family, species) %>%
   arrange(order, family, species) %>%
   unique()
-  
+
 # List of collection and institutions
 collection_institution_df <- do.collection.institution.table(data)
 
@@ -78,66 +153,47 @@ species_references_df <-
 # First and last record of each species
 species_record_df <-
   data %>%
-  mutate(
-    International = ifelse(
-      International == "",
-      "NE",
-      International),
-    Nacional = ifelse(
-      Nacional == "",
-      "NE",
-      Nacional),
-    Regional.BA = ifelse(
-      Regional.BA == "",
-      "NE",
-      Regional.BA),
-    Regional.ES = ifelse(
-      Regional.ES == "",
-      "NE",
-      Regional.ES)
-  ) %>%
-  filter(!str_detect(scientificName, "Felis"), !is.na(scientificName)) %>%
+  filter(!is.na(year), year != "", year != "NA") %>%
   group_by(species) %>%
   summarise(
-    scientificName = unique(scientificName),
-    first_record = min(as.numeric(year), na.rm = TRUE),
-    last_record = max(as.numeric(year), na.rm = TRUE),
-    International = unique(International),
-    Nacional = unique(Nacional),
-    Regional.BA = unique(Regional.BA),
-    Regional.ES = unique(Regional.ES)
+    first_record = min(year),
+    last_record = max(year),
+    IUCN = unique(IUCN),
+    ICMBio = unique(ICMBio)
   ) %>%
   arrange(by = last_record)
 
-# Plot -------------------------------------------------------
+# Plot ------------------------------------------------------------------------
 
-first_record_df <-
-  select(species_record_df, first_record)
-last_record_df <-
-  select(species_record_df, last_record)
-
-colnames(first_record_df) <- "Year"
-colnames(last_record_df) <- "Year"
-
-first_record_df$id <- "First record"
-last_record_df$id <- "Last record"
-
-records_df <- bind_rows(first_record_df, last_record_df)
-
-plot_through_years <-
-  ggplot(records_df, aes(x = Year, fill = id)) +
-  geom_histogram() +
+frst_lst_rcrd_graph <-
+  data %>%
+  filter(!is.na(year), year != "", year != "NA") %>%
+  mutate(year = as.Date(as.character(year), "%Y")) %>%
+  group_by(species) %>%
+  summarise(
+    first_record = min(year),
+    last_record = max(year)
+  ) %>%
+  pivot_longer(
+    cols = -c(species),
+    names_to = "record",
+    values_to = "year"
+  ) %>%
+  ggplot() +
+  scale_x_date(date_labels = "%Y") +
+  scale_fill_discrete(labels = c("First record", "Last record")) +
+  geom_histogram(aes(x = year, fill = record)) +
   theme_light() +
   ylab("Number of species") +
   xlab("Years") +
   theme(legend.title = element_blank())
 
-# Save -------------------------------------------------------
+# Save ------------------------------------------------------------------------
 # Plot
-plot_through_years
+frst_lst_rcrd_graph
 ggsave("../data/results/first-last-record-plot.pdf",
-  width = 5,
-  height = 4
+  width = 8,
+  height = 46
 )
 
 # Tables
