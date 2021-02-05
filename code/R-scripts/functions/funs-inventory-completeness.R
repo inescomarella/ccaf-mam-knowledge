67f52d28c7cc9f16f8b44eeae222ae3b7bf08734
InesComarella <inesmottacomarella@gmail.com>
2021-02-05 12:54:43 -0300

    :sparkles: New function
    Calculate research insitutes proximity impact

--- code/R-scripts/functions/funs-inventory-completeness.R
+++ code/R-scripts/functions/funs-inventory-completeness.R
@@ -46,3 +46,71 @@ get.nearest.dist <- function(pts, polygon) {
   
   st_as_sf(polygon)
 }
+
+
+research.institute.impact <- function(grid, institutes_points) {
+  utm <-
+    CRS("+proj=utm +zone=24 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  
+  # Set crs
+  institutes_points <- st_transform(institutes_points, utm)
+  grid <- st_transform(grid, utm)
+  
+  grid_ids <- unique(grid$grid_id)
+  
+  # Get research institutes names
+  institutes <- institutes_points$institution_name
+  
+  pri_df <- data.frame()
+  for (i in 1:length(institutes)) {
+    for (j in 1:length(grid_ids)) {
+      
+      # Total number of records per grid cell
+      nrec_j <- grid %>%
+        group_by(grid_id) %>%
+        filter(
+          grid_id == grid_ids[j]
+        ) %>%
+        nrow()
+      
+      # Number of records from a certain research institute per grid cell
+      nrec_ij <-
+        grid %>%
+        group_by(grid_id) %>%
+        filter(
+          grid_id == grid_ids[j] &
+            institutionCode == institutes[i]
+        ) %>%
+        nrow()
+      
+      # Proximity to research institute data.frame
+      dist_ij <-
+        st_distance(
+          x = st_geometry(filter(grid, grid_id == grid_ids[j]))[1],
+          y = st_geometry(institutes_points)[i]
+        )
+      
+      # Relative contribution of a certain research institute
+      rel_contrib_j <- nrec_ij / nrec_j
+      
+      if(is.na(as.numeric(dist_ij)) | as.numeric(dist_ij) == 0){
+        dist_ij <- 1
+      }
+      
+      pri_df[j, i] <- rel_contrib_j / as.numeric(dist_ij)
+    }
+  }
+  
+  pri_impact <- pri_df %>%
+    # Remove columns with sum = 0
+    select(which(!colSums(pri_df, na.rm = TRUE) %in% 0)) %>%
+    mutate(grid_id = grid_ids) %>%
+    rowwise() %>%
+    
+    # Sum relative contribution * proximity of each institute per grid cell
+    mutate(impact = sum(c_across(-c(grid_id)), na.rm = TRUE)) %>%
+    select(grid_id, impact)
+  
+  
+  merge(grid, pri_impact, by = "grid_id")
+}
