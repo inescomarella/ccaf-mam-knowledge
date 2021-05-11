86e3f2b524b536238eeef9b47cacf356a4e04111
InesComarella <inesmottacomarella@gmail.com>
2021-05-11 17:08:33 -0300

    :adhesive_bandage: Improve plots
    Remove sources bar plot, improve first-last records graph

--- scripts/04_plot_data_sources.R
+++ scripts/04_plot_data_sources.R
@@ -47,24 +47,6 @@ colection_contri %>%
 
 # Plot ------------------------------------------------------------
 
-bar_graph <- colection_contri %>%
-  ggplot() +
-  geom_bar(aes(
-    x = factor(Collection, levels = level_order$Collection),
-    y = n
-  ),
-  stat = "identity",
-  width = 0.8) +
-  theme_light() +
-  labs(
-    color = element_blank(),
-    x = element_blank(),
-    y = "Total number of records"
-  ) +
-  theme(legend.text=element_text(size=15),
-        axis.text = element_text(size=15),
-        axis.title = element_text(size=15))
-
 line_graph_collections <- records_df %>%
   filter(year != "NA") %>%
   mutate(fk_group = "a") %>%
@@ -85,8 +67,7 @@ line_graph_collections <- records_df %>%
         axis.text = element_text(size=15),
         axis.title = element_text(size=15))
 
-frst_lst_rcrd_graph <-
-  data %>%
+frst_lst_rcrd_df <- data %>%
   filter(!is.na(year), year != "", year != "NA") %>%
   mutate(year = as.Date(as.character(year), "%Y")) %>%
   group_by(species) %>%
@@ -99,17 +80,44 @@ frst_lst_rcrd_graph <-
     names_to = "record",
     values_to = "year"
   ) %>%
-  ggplot() +
+  group_by(year, record) %>%
+  summarise(n = n()) %>%
+  mutate(n = ifelse(record == "last_record", -n, n))
+
+yseq <- seq(1810, 2030, 10)
+
+frst_lst_rcrd_df_10y <- data.frame()
+
+for (i in 1:length(yseq)) {
+    df_10y <- frst_lst_rcrd_df %>%
+      filter(year >= as.Date(as.character(yseq[i]), "%Y") && year < as.Date(as.character(yseq[i + 1]), "%Y")) %>%
+      group_by(record) %>%
+      summarise(n = sum(n)) %>%
+      mutate(year = as.Date(as.character(yseq[i]), "%Y"))
+
+    frst_lst_rcrd_df_10y <- bind_rows(frst_lst_rcrd_df_10y, df_10y)
+}
+
+breaks_values <- 
+  pretty(frst_lst_rcrd_df_10y$n, n = 10)
+
+frst_lst_rcrd_graph <- frst_lst_rcrd_df_10y %>%
+  ggplot(aes(x = year, y = n, fill = record)) +
   scale_x_date(date_labels = "%Y") +
+  geom_hline(yintercept = 0)+
   scale_fill_discrete(labels = c("First record", "Last record")) +
-  geom_histogram(aes(x = year, fill = record)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(breaks = breaks_values,
+                     labels = abs(breaks_values)) +
   theme_light() +
   ylab("Number of species") +
   xlab(element_blank()) +
-  theme(legend.title = element_blank(),
-        legend.text=element_text(size=15),
-        axis.text = element_text(size=15),
-        axis.title = element_text(size=15))
+  theme(
+    legend.title = element_blank(),
+    legend.text = element_text(size = 15),
+    axis.text = element_text(size = 15),
+    axis.title = element_text(size = 15)
+  )
 
 # Save ------------------------------------------------------------
 # Plot
@@ -119,15 +127,11 @@ ggsave("figs/04_first_last_record.png",
   height = 6
 )
 
-plot_grid(bar_graph, line_graph_collections, 
-          ncol = 2, 
-          labels = c("(a)", "(b)"), 
-          label_size = 15)
-
+line_graph_collections
 ggsave("figs/04_data_sources.png",
-       width = 16,
+       width = 12,
        height = 6
 )
 
 # Save workspace ----
-save.image("~/tcc-ccma/code/workspaces/explore_data.RData")
+save.image("~/tcc-ccma/workspaces/explore_data.RData")
